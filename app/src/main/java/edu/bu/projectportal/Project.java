3f1d19a2c02b8af54376a67e0ea733ee32321570
Chad David Cover <chad.cover@cengage.com>
2018-11-15 19:50:08 -0500

    merged changes from lab 2 are working

--- app/src/main/java/edu/bu/projectportal/Project.java
+++ app/src/main/java/edu/bu/projectportal/Project.java
@@ -1,18 +1,104 @@
 package edu.bu.projectportal;
 
+import static android.util.Half.NaN;
+
 /**
  * Created by danazh on 4/23/18.
+ * Revised by Chad David Cover on 11/16/2018.
  */
 
 public class Project {
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // member variables
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
     private int id;
     private String title;
     private String summary;
-    public  static final Project[] projects = {
-            new Project("Weather Forecast", "Weather Forcast is an app ..."),
-            new Project("Connect Me", "Connect Me is an app ... "),
-            new Project("What's Eat", "What's Eat is an app ..."),
-            new Project("Project Portal", "Project Portal is an app ...")};
+    private String[] authors;
+    private String[] links;
+    private boolean favorite;
+    private String[] keywords;
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // sample data
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    private static String[] sampleAuthors = {"Larry", "Curly", "Moe"};
+    private static String[] sampleAuthors2 = {"Fred Flintstone", "Wilma Flintstone", "Barney Rubble", "Betty Rubble"};
+    private static String[] sampleAuthors3 = {"Bill Haywood", "Emma Goldman", "Eugene V. Debs"};
+    private static String[] sampleAuthors4 = {"John Jay", "James Madison", "Alexander Hamilton"};
+
+    private static String[] sampleLinks = {"http://google.com", "http://bu.edu"};
+    private static String[] sampleLinks2 = {"http://stackoverflow.com", "http://regexr.com"};
+    private static String[] sampleLinks3 = {"http://github.com", "http://w3schools.com"};
+    private static String[] sampleLinks4 = {"http://npmjs.com", "http://sass-lang.com"};
+
+    private static String[] sampleKeywords = {"Java", "XML", "Android"};
+    private static String[] sampleKeywords2 = {"Javascript", "Node", "Angular"};
+    private static String[] sampleKeywords3 = {"PHP", "MySQL", "Apache"};
+    private static String[] sampleKeywords4 = {"Perl", "XML", "DOM"};
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // constructors
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    public Project(int id) {
+        this.id = id;
+        this.title = "Sample Project";
+        this.summary = "Sample Project is an app...";
+        this.authors = this.sampleAuthors;
+        this.links = this.sampleLinks;
+        this.favorite = false;
+        this.keywords = sampleKeywords;
+    }
+
+    public Project(int id, String title, String summary) {
+        this.id = id;
+        this.title = title;
+        this.summary = summary;
+        this.authors = this.sampleAuthors;
+        this.links = this.sampleLinks;
+        this.favorite = false;
+        this.keywords = sampleKeywords;
+    }
+
+    public Project(int id, String title, String summary, String[] authors, String[] links,
+                   boolean favorite, String[] keywords) {
+        this.id = id;
+        this.title = title;
+        this.summary = summary;
+        this.authors = authors;
+        this.links = links;
+        this.favorite = favorite;
+        this.keywords = keywords;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // instantiation of sample product array
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    public final static Project[] projects = {
+            new Project(1, "Weather Forecast", "Weather Forecast is an app ...",
+                    sampleAuthors, sampleLinks, false, sampleKeywords),
+            new Project (2, "Connect Me", "Connect Me is an app ... ",
+                    sampleAuthors2, sampleLinks2, false, sampleKeywords2),
+            new Project(3, "What's Eat", "What's Eat is an app ...",
+                    sampleAuthors3, sampleLinks3, false, sampleKeywords3),
+            new Project (4, "Project Portal", "Project Portal is an app ...",
+                    sampleAuthors4, sampleLinks4, true, sampleKeywords4),
+            new Project(5),
+    };
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // accessors & mutators
+    ///////////////////////////////////////////////////////////////////////////////////////////
 
     public final int getId() {
         return this.id;
@@ -38,19 +124,46 @@ public class Project {
         this.summary = summary;
     }
 
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+
+    public String[] getLinks() {
+        return links;
+    }
+
+    public void setLinks(String[] links) {
+        this.links = links;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
+
+    public String[] getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String[] keywords) {
+        this.keywords = keywords;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // helper methods
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+
     public String toString() {
         return "Project{title='" + this.title +
                 '\'' + ", summary='" + this.summary + '\'' + '}';
     }
 
-    public Project(String title, String summary) {
-        this.title = title;
-        this.summary = summary;
-    }
-
-    public Project(int id, String title, String summary) {
-        this.id = id;
-        this.title = title;
-        this.summary = summary;
-    }
 }
--- app/src/main/java/edu/bu/projectportal/ProjectDetailFragment.java
+++ app/src/main/java/edu/bu/projectportal/ProjectDetailFragment.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 
@@ -14,23 +16,46 @@ import android.widget.TextView;
  */
 public class ProjectDetailFragment extends Fragment {
 
-    int projectId;
-    TextView titleTextView, summaryTextView;
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // member variables
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+    protected int projectId;
+    protected TextView titleTextView, summaryTextView, authorsTextView, linksTextView, keywordsTextView;
+    protected Switch switchButton;
+    protected Boolean shouldRun = false;
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // constructors
+    ///////////////////////////////////////////////////////////////////////////////////////////
 
     public ProjectDetailFragment() {
-        // Required empty public constructor
+        // required. can be empty.
     }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // required overrides
+    ///////////////////////////////////////////////////////////////////////////////////////////
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
 
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_project_detail, container, false);
 
+        ///////////////////////////////////////////////////////////////////////////////////////////
+        // view mappings
+        ///////////////////////////////////////////////////////////////////////////////////////////
         titleTextView = view.findViewById(R.id.projTitleTextViewId);
         summaryTextView = view.findViewById(R.id.projSummaryTextViewId);
-
+        authorsTextView = view.findViewById(R.id.projAuthorsTextViewId);
+        linksTextView = view.findViewById(R.id.projLinksTextViewId);
+        keywordsTextView = view.findViewById(R.id.projKeywordsTextViewId);
+        switchButton = view.findViewById(R.id.switchButton);
 
         if (getArguments()!= null)
             projectId = getArguments().getInt("projectid");
@@ -41,16 +66,76 @@ public class ProjectDetailFragment extends Fragment {
         return view;
     }
 
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // accessors and mutators
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
     public void setProject(int projId) {
 
         projectId = projId;
 
         titleTextView.setText(projId + ":" + Project.projects[projId].getTitle());
         summaryTextView.setText(Project.projects[projId].getSummary());
+
+        String[] authors = Project.projects[projectId].getAuthors();
+        String authorsConcat = this.concatString(authors);
+        authorsTextView.setText(authorsConcat);
+
+        String[] links = Project.projects[projectId].getLinks();
+        String linksConcat = this.concatString(links);
+        linksTextView.setText(linksConcat);
+
+        String[] keywords = Project.projects[this.projectId].getKeywords();
+        String keywordsConcat = this.concatString(keywords);
+        keywordsTextView.setText(keywordsConcat);
+
+        boolean isFavorite = Project.projects[projectId].isFavorite();
+        switchButton.setChecked(isFavorite);
+
+        switchButton.setTag("DONOTRUN");
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+
+                if (switchButton.getTag() != null) {
+                    switchButton.setTag(null);
+                    return;
+                }
+                changeFavorite();
+            }
+        });
+
+
+
     }
 
     public int getProjectId(){
         return projectId;
+    }
+
+
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    // helper methods
+    ///////////////////////////////////////////////////////////////////////////////////////////
+
+
+    public void changeFavorite() {
+        boolean isFavorite = Project.projects[projectId].isFavorite();
+        Project.projects[projectId].setFavorite(!isFavorite);
+    }
+
+
+    public String concatString(String[] stringArray) {
+        String string = "";
+        for (int i = 0; i < stringArray.length; i++ ) {
+            if (i != (stringArray.length - 1) ) {
+                string += (stringArray[i] + ", ");
+            } else {
+                string += stringArray[i];
+            }
+        }
+        return string;
     }
 
 
